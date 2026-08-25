4a19c6be24f18e96f31d3b9c5091ce3770fddb96
tanneauthomas56@gmail.com <tanneauthomas56@gmail.com>
2026-08-25 15:58:40 +0200

    [Feature] Create HomePage fragment with layout and ViewModel setup

---
+++ app/src/main/java/com/wamt/ui/home/HomePage.java
@@ -0,0 +1,38 @@
+package com.wamt.ui.home;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wamt.R;
+
+public class HomePage extends Fragment {
+
+    private HomePageViewModel mViewModel;
+
+    public static HomePage newInstance() {
+        return new HomePage();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HomePageViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
